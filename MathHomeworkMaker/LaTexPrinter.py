05ad5a499bef69173f505e506d1906e20a701b67
Yifan Yao <fanfannnmn@gmail.com>
2019-05-03 11:58:01 -0400

    Syntax optimization

--- MathHomeworkMaker/LaTexPrinter.py
+++ MathHomeworkMaker/LaTexPrinter.py
@@ -7,10 +7,10 @@ import time
 
 # Generate Problems
 numberOfQuestions = input('Please Enter Number of Questions: ')
-fileName = time.strftime("%Y%m%d%H%M%S")
+fileName = 'ProblemSet' + time.strftime("%Y%m%d%H%M%S")
 
-f = open('output/ProblemSet' + fileName + '.tex', 'w')
-fa = open('output/ProblemSet' + fileName + 'Ans.tex', 'w')
+f = open('output/' + fileName + '.tex', 'w')
+fa = open('output/' + fileName + 'Ans.tex', 'w')
 
 temp = open('template/head', 'r')
 for line in temp:
@@ -32,17 +32,13 @@ fa.close()
 current = os.getcwd()
 temp = tempfile.mkdtemp()
 os.chdir(temp)
+printout = [fileName, fileName + 'Ans']
 
-proc = subprocess.Popen(['pdflatex', current + '/output/ProblemSet' + fileName + '.tex'])
-subprocess.Popen(['pdflatex', current + '/output/ProblemSet' + fileName + '.tex'])
-proc.communicate()
+for x in printout:
+    proc = subprocess.Popen(['pdflatex', current + '/output/' + x + '.tex'])
+    subprocess.Popen(['pdflatex', current + '/output/' + x + '.tex'])
+    proc.communicate()
 
-shutil.copy('ProblemSet' + fileName + '.pdf', current + '/output')
-
-proc = subprocess.Popen(['pdflatex', current + '/output/ProblemSet' + fileName + 'Ans.tex'])
-subprocess.Popen(['pdflatex', current + '/output/ProblemSet' + fileName + 'Ans.tex'])
-proc.communicate()
-
-shutil.copy('ProblemSet' + fileName + 'Ans.pdf', current + '/output')
+    shutil.copy(x + '.pdf', current + '/output')
 
 shutil.rmtree(temp)
